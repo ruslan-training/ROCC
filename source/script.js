33cd98ce6ed9defda0918d1630556e5d05a4312f
kbnd <devkbnd@gmail.com>
2021-10-16 01:52:12 +0300

    header

---
+++ source/script.js
@@ -0,0 +1,36 @@
+let burger = document.querySelector('.burger'),
+    adaptivemenu = document.querySelector('.adaptive-menu'),
+    lists = document.querySelectorAll('.list__item'),
+    body = document.body;
+
+burger.addEventListener('click', function(){
+    burger.classList.toggle('_active');
+    adaptivemenu.classList.toggle('_active');
+    body.classList.toggle('_lock');
+    for(list of lists) {
+        list.classList.toggle('_active');
+    }
+})
+
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 100, // offset (in px) from the original trigger point
+    delay: 300, // values from 0 to 3000, with step 50ms
+    duration: 700, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
